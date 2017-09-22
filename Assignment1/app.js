68ebe86cfa64d4663945b68dc8245d2e8bdcde9b
Beverly Jean-Baptiste <beverlyj18@hotmail.com>
2017-09-21 20:59:35 -0500

    Removed Query to DOM

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -15,34 +15,27 @@
         return count;
     }
     
-    function styleOutput(selector, count) {
-        if (count === 0) {
-            selector.style.color = "red";
-            selector.style.border = "1px solid red";
-        } else {
-            selector.style.color = "green";
-            selector.style.border = "1px solid green";
-        }
-    }
-    
     function LunchCheckController($scope) {
         $scope.menu = "";
         $scope.message = "";
+        $scope.textbox = {};
         $scope.checkMenu = function () {
             var menuItems = $scope.menu.split(','),
                 emptyItems = getEmptySpaces(menuItems),
-                finalCount = menuItems.length - emptyItems,
-                textbox = document.querySelector("#textbox");
+                finalCount = menuItems.length - emptyItems;
             
             if (finalCount === 0) {
                 $scope.message = "Please enter data first";
-                styleOutput(textbox, 0);
-            } else if (finalCount <= 3) {
-                $scope.message = "Enjoy!";
-                styleOutput(textbox, 1);
+                $scope.textbox = { "color": "red", "border": "1px solid red"};
             } else {
-                $scope.message = "Too much!";
-                styleOutput(textbox, 1);
+                $scope.textbox = { "color": "green", "border": "1px solid green"};
+                
+                if (finalCount <= 3) {
+                    $scope.message = "Enjoy!";
+                
+                } else {
+                    $scope.message = "Too much!";
+                }
             }
         };
     }
